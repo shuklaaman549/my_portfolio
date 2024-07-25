d8757664f672a6068159396b4bd5b9618853f56b
shuklaaman549 <shuklaaman549@gmail.com>
2024-07-25 18:10:59 +0530

    Initial commit

---
+++ components/Social.jsx
@@ -0,0 +1,27 @@
+import { Item } from "@radix-ui/react-select";
+import Link from "next/link";
+
+import { FaGithub, FaLinkedinIn, FaTwitter, FaInstagram } from "react-icons/fa";
+
+const socials = [
+  { icon: <FaGithub />, path: "" },
+  { icon: <FaLinkedinIn />, path: "" },
+  { icon: <FaInstagram />, path: "" },
+  { icon: <FaTwitter />, path: "" },
+];
+
+const Social = ({ containerStyles, iconStyles }) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, index) => {
+        return (
+          <Link key={index} href={item.path} className={iconStyles}>
+            {item.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Social;
